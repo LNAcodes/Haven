af02bef9f4dedfd77bc83ac23d2e1c494a1c430e
LNAcodes <182744940+LNAcodes@users.noreply.github.com>
2026-03-18 15:18:22 +0100

    wip: install next-auth, configure GitHub OAuth and add auth to incidents API

--- pages/api/incidents/index.js
+++ pages/api/incidents/index.js
@@ -2,14 +2,27 @@
 
 import dbConnect from "@/db/connect";
 import Incident from "@/models/Incident";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
+import { getToken } from "next-auth/jwt";
 
 export default async function handler(request, response) {
   await dbConnect();
 
+  const session = await getServerSession(request, response, authOptions);
+  const token = await getToken({ req: request });
+  const userId = token?.sub;
+
   switch (request.method) {
     case "GET": {
       try {
-        const incidents = await Incident.find().sort({ date: -1 });
+        if (!session) {
+          response.status(401).json({ message: "Unauthorized" });
+          break;
+        }
+        const incidents = await Incident.find({ userId }).sort({
+          date: -1,
+        });
         response.status(200).json(incidents);
         break;
       } catch (error) {
@@ -19,6 +32,9 @@ export default async function handler(request, response) {
     }
     case "POST": {
       try {
+        if (!session) {
+          return response.status(401).json({ message: "Unauthorized" });
+        }
         const {
           involvedPersons,
           witnesses: witnessesInput,
@@ -42,6 +58,7 @@ export default async function handler(request, response) {
           : [];
 
         const incident = await Incident.create({
+          userId,
           involvedPersons: involvedPersonsArray,
           witnesses: witnessesArray,
           date,
